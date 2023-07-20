8540a89e0a80b3c3f9326a629c99c80c0b234df7
eighty-nine89 <awueteysamuel222@yahoo.com>
2023-07-20 18:04:20 +0000

    categories

--- components/categories.js
+++ components/categories.js
@@ -1,7 +1,10 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Image } from 'react-native'
+import { categories } from '../screens/constants'
 
 export default function Categories() {
+    const [activeCategory, setActiveCategory] = useState(null);
   return (
     <View className="mt-4">
       <ScrollView
@@ -12,7 +15,24 @@ export default function Categories() {
         paddingHorizontal: 15
       }}
     >
-       
+       {
+          categories.map((category, index)=>{
+            let isActive = category.id==activeCategory;
+            let btnClass = isActive? ' bg-gray-600' : 'bg-gray-200';
+            let textClass = isActive? ' font-semibold text-gray-800': ' text-gray-500'
+            return (
+              <View key={index} className="flex justify-center items-center mr-6">
+                <TouchableOpacity
+                onPress={()=> setActiveCategory(category.id)}
+                className="p-1 rounded-full shadow bg-gray-200">
+                  <Image style={{width: 45, height: 45}}
+                  source={category.image} />
+                  <Text className="text-sm">{category.name}</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          })
+       }
     </ScrollView>
     </View>
   )
